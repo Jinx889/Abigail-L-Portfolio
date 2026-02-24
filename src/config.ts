af994d5ba87d1e9bb1daaa3e1325b5be2d45d53a
Abigail Lopez <138689870+Jinx889@users.noreply.github.com>
2026-02-24 12:44:37 -0600

    Modify site
    Added new user details and projects.

--- src/config.ts
+++ src/config.ts
@@ -1,34 +1,30 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Abigail Lopez",
+  description: "Portfolio website of Abigail Lopez",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "abigail.lopez0011@gmail.com",
+    linkedin: "https://www.linkedin.com/in/abigail-lopez-86841b238/",
+    github: "https://github.com/Jinx889",
   },
   aboutMe:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
-  skills: ["Javascript", "React", "Node.js", "Python", "AWS", "Docker"],
+    "Hello! I'm Abigail Lopez, a recent graduate with a Bachelor of Science in Computer Science and a co-major in English. As I step into the exciting world of technology, I'm fueled by an insatiable curiosity and a deep love for learning. Throughout my academic journey, I've had the privilege of working with Java, Python, and C#, languages that have not only become tools in my toolkit but also sources of fascination and endless possibilities. I've had the pleasure of working on projects that merge technology and creativity, such as developing games using Python's Pygame library and crafting virtual reality (VR) prototypes in Unity. These experiences have reinforced my belief that technology can be a canvas for artistic expression, and I'm eager to continue pushing the boundaries of what's possible. As I prepare to take the next step in my career, I'm excited about the prospect of contributing to innovative projects that leverage technology to create captivating and meaningful experiences. I'm equally eager to connect with professionals who share my enthusiasm for technology, creativity, and collaboration.",
+  skills: ["Javascript", "Python", "HMTL", "CSS", "GD Script", "C#"],
   projects: [
     {
-      name: "AI Dev Roundup Newsletter",
+      name: "Space Training Simulator",
       description:
-        "One concise email. Five minutes. Every Tuesday. Essential AI news & trends, production-ready libraries, powerful AI tools, and real-world code examples",
-      link: "https://aidevroundup.com/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
+        " small game made in Bitsy. Play as a pilot going through a spaceflight training simulation. Features two endings and art, music, writing, and level-design completed by me in a week.",
+      link: "https://jinx001.itch.io/space-exploration-training-simulator",
     },
     {
-      name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
+      name: "A Month to Mars",
       description:
-        "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
+        "A small resource management game featuring, moral quandaries, three resources to manage, and a time constraint of 30 days! A linear game that was completely created by me in a month.",
       link: "https://fullstackextensions.com/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
     },
     {
-      name: "ExtensionKit",
+      name: "",
       description:
         "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
       link: "https://extensionkit.io/?ref=devportfolio",
